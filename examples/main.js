ccfb149ac355e40abdebb944b1614a9bf7c4cbc8
yaoxfly <1315892088@qq.com>
2019-07-25 19:31:15 +0800

    '调整目录结构'

---
+++ examples/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import yxRequest from './request'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  yxRequest,
+  render: h => h(App)
+}).$mount('#app')
---
+++ examples/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo"
+         src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+
+  created () {
+    this.$yxRequest.requests('test/test', { name: '1', age: '1' }, 'put', false).then(res => {
+      console.log(res)
+    })
+  }
+}
+</script>
