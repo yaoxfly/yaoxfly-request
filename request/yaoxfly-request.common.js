847b913f0778716a7bb6c810efa962c975635690
yaoxfly <1315892088@qq.com>
2019-07-26 19:01:51 +0800

    1

--- request/yaoxfly-request.common.js
+++ request/yaoxfly-request.common.js
@@ -2019,11 +2019,11 @@ lib_request.install = function (Vue) {
       var options = this.$options; // vue的option 方法
       // 如果存在
 
-      if (options.yxRequest) {
+      if (options.YxRequest) {
         // 混入vue实例的名字 定好就不可更改 在vue实例中添加  在new Vue()中添加 也要 vue.use()
-        this.$yxRequest = options.yxRequest; // 为这个值赋值为this.$yxRequest
-      } else if (options.parent && options.parent.$yxRequest) {
-        this.$yxRequest = options.parent.$yxRequest;
+        this.$YxRequest = options.YxRequest; // 为这个值赋值为this.$yxRequest
+      } else if (options.parent && options.parent.$YxRequest) {
+        this.$YxRequest = options.parent.$YxRequest;
       }
     }
   });
